0690117fbd2096396cc4478347488e3dbb2aa701
happy-v587 <code4happy@gmail.com>
2026-07-14 11:02:52 +0800

    refactor(cmd): add typed flush commands

--- src/cmd/src/flushdb.rs
+++ src/cmd/src/flushdb.rs
@@ -21,7 +21,7 @@ use client::Client;
 use resp::RespData;
 use storage::storage::Storage;
 
-use crate::{AclCategory, ClientExt, Cmd, CmdFlags, CmdMeta};
+use crate::{AclCategory, ClientExt, Cmd, CmdFlags, CmdMeta, CommandResult};
 use crate::{impl_cmd_clone_box, impl_cmd_meta};
 
 #[derive(Clone, Default)]
@@ -67,5 +67,21 @@ impl Cmd for FlushdbCmd {
                 client.set_storage_error(&e);
             }
         }
+    }
+
+    fn execute_typed(&self, client: &Client, storage: Arc<Storage>) -> Option<CommandResult> {
+        Some(match client.argv().len() {
+            1 => match storage.flushdb() {
+                Ok(()) => Ok(RespData::SimpleString("OK".into())),
+                Err(error) => Err(crate::error::CommandError::storage(error)),
+            },
+            _ => Err(crate::error::CommandError::WrongArity {
+                command: self.name().to_string(),
+            }),
+        })
+    }
+
+    fn uses_typed_execution(&self) -> bool {
+        true
     }
 }
