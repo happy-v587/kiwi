6bd9d72811a9c591eebef9bafe206e96f250b312
happy-v587 <code4happy@gmail.com>
2026-07-14 11:04:38 +0800

    refactor(cmd): add typed getset execution

--- src/cmd/src/getset.rs
+++ src/cmd/src/getset.rs
@@ -21,7 +21,7 @@ use client::Client;
 use resp::RespData;
 use storage::storage::Storage;
 
-use crate::{AclCategory, ClientExt, Cmd, CmdFlags, CmdMeta};
+use crate::{AclCategory, ClientExt, Cmd, CmdFlags, CmdMeta, CommandResult};
 use crate::{impl_cmd_clone_box, impl_cmd_meta};
 
 #[derive(Clone, Default)]
@@ -82,5 +82,24 @@ impl Cmd for GetsetCmd {
             }
             Err(e) => client.set_storage_error(&e),
         }
+    }
+
+    fn execute_typed(&self, client: &Client, storage: Arc<Storage>) -> Option<CommandResult> {
+        let argv = client.argv();
+        Some(match argv.len() {
+            3 => match storage.getset(&argv[1], &argv[2]) {
+                Ok(value) => Ok(RespData::BulkString(
+                    value.map(|value| value.into_bytes().into()),
+                )),
+                Err(error) => Err(crate::error::CommandError::storage(error)),
+            },
+            _ => Err(crate::error::CommandError::WrongArity {
+                command: self.name().to_string(),
+            }),
+        })
+    }
+
+    fn uses_typed_execution(&self) -> bool {
+        true
     }
 }
